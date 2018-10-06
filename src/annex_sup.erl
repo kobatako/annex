a91d0f5892cfa3e8c540923203f89c98c6188ef3
kobaru <kobaru@kobaru.local.home>
2018-10-06 13:03:32 +0900

    feature sample echo server

--- src/annex_sup.erl
+++ src/annex_sup.erl
@@ -19,6 +19,7 @@
 %% API functions
 %%====================================================================
 
+-spec start_link() -> {ok, pid()}.
 start_link() ->
     supervisor:start_link({local, ?SERVER}, ?MODULE, []).
 
